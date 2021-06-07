6e2b13f6c1301f5b2a9cc9a8879017c91d15b490
Karen <karenymorel@gmail.com>
2021-06-07 10:07:44 -0300

    Basic Style and Functionality

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 "use strict";
 
-const newTodo = document.querySelector("#newtodo");
-const addBtn = document.querySelector(".add-btn");
+const addBtn = document.querySelector(".add");
+const clearBtn = document.querySelector(".clear");
+const list = document.querySelector("ul");
+
+addBtn.addEventListener("click", function () {
+	const newTodo = document.getElementById("newtodo").value.trim();
+	if (newTodo != "") {
+		document.querySelector("ul").insertAdjacentHTML("beforeend", `<li><span>${newTodo}</span><button class="done-btn">Done</button> <button class="delete-btn">X</button></li>`);
+		document.getElementById("newtodo").value = "";
+	}
+});
+
+list.addEventListener("click", function(e){
+  if(e.target.classList.contains("done-btn")){
+    e.target.parentElement.firstElementChild.classList.toggle("finished");
+  };
+
+  if(e.target.parentElement.firstElementChild.classList.contains("finished")){
+    e.target.parentElement.firstElementChild.style.textDecoration = "line-through";
+  } else {
+    e.target.parentElement.firstElementChild.style.textDecoration = "none";
+  };
+});
+
+list.addEventListener("click", function (e) {
+  if(e.target.classList.contains("delete-btn")){
+    e.target.parentElement.remove();
+  }
+});
+
+clearBtn.addEventListener("click", function(e){
+  list.innerHTML = "";
+})
